db4f082b4ebce831584d04d96aeaa523823c955f
jasonyang2023 <182254440@qq.com>
2025-07-18 11:15:00 +0800

    feat: init

---
+++ eslint.config4.js
@@ -0,0 +1,39 @@
+import antfu from '@antfu/eslint-config'
+
+export default antfu(
+  {
+    type: 'app', // 项目类型为 app/lib
+
+    stylistic: {
+      indent: 2, // 缩进风格
+      quotes: 'single', // 单引号
+    },
+
+    typescript: true,
+    vue: {
+      overrides: {
+        // enforce order of component top-level elements 自定义 Vue 文件中标签的顺序，模板 -> 脚本 -> 样式
+        'vue/block-order': [
+          'error',
+          {
+            order: ['template', 'script', 'style'],
+          },
+        ],
+      },
+    },
+    jsonc: false,
+    yaml: false,
+    ignores: [
+      '**/fixtures', // 忽略特定路径下的文件（如 fixtures 目录）
+    ],
+  },
+
+  // TypeScript 文件的规则配置
+  {
+    files: ['**/*.ts'], // 仅匹配 TypeScript 文件
+    rules: {},
+  },
+  {
+    rules: {},
+  },
+)
---
+++ src/main.ts
@@ -0,0 +1,11 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import './style.css'
+
+function test() {
+  console.log('test')
+}
+const a = 'xx'
+
+console.log(a)
+createApp(App).mount('#app')
