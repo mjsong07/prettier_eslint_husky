692d11b292988e602e151ea3bb5e2d98e66718c6
jasonyang2023 <182254440@qq.com>
2025-07-18 11:42:22 +0800

    feat: init2

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import './style.css'
 
-const a = 'xx'
-const aa = 'xx'
+const a = 'xx' 
+const ab = 'xx' 
 // aa
 console.log(a)
 createApp(App).mount('#app')
